b8249714bd202976b2a617e421d12cbd4955fc97
Sajede <m.sajede1375@gmail.com>
2019-06-24 17:24:01 +0430

    set vue-router,vue-resource, vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
+
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+
+import { store } from './store/store';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'main URL';
+
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
